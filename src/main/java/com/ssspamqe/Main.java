75984ac83e1a939d93b2865a5b4af626f0e790de
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-04-14 21:26:50 +0300

    Add properties configuration

--- src/main/java/com/ssspamqe/Main.java
+++ src/main/java/com/ssspamqe/Main.java
@@ -1,15 +1,16 @@
 package com.ssspamqe;
 
 import com.ssspamqe.fractalgeneration.fractalcreators.MultiThreadFractalCreator;
-import com.ssspamqe.fractalgeneration.fractalcreators.SingleThreadFractalCreator;
 import com.ssspamqe.fractalgeneration.graphics.PixelCanvas;
 import com.ssspamqe.fractalgeneration.pointmodifiers.AffineTransformation;
 import com.ssspamqe.fractalgeneration.pointmodifiers.pointfunctions.DiscFunction;
-import com.ssspamqe.fractalgeneration.pointmodifiers.pointfunctions.PolarFunction;
 import com.ssspamqe.fractalgeneration.pointmodifiers.pointfunctions.SinusoidalFunction;
-import com.ssspamqe.imagerender.FileFormat;
 import com.ssspamqe.imagerender.ImageRenderer;
 import com.ssspamqe.imagerender.JavaGraphicsImageRenderer;
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.inspector.TagInspector;
 
 import java.awt.*;
 import java.nio.file.Path;
@@ -22,36 +23,57 @@ public class Main {
 
     private static final int MAX_COLOR = 255;
     private static final int MAX_ABSOLUTE_SHIFT = 20;
+    private static final String PROPERTIES_FILE_PATH = "generation.yaml";
 
-    public static void main(String[] args) {
-        List<AffineTransformation> affineTransformations = buildListOfAffineTransformations(70);
+    public void main(String[] args) {
+        var properties = getProperties();
+
+        List<AffineTransformation> affineTransformations =
+                buildListOfAffineTransformations(
+                        properties.getAffineTransformations(),
+                        properties.getAffineTransformationsGenerationTryouts()
+                );
+
         var fractalGenerator = MultiThreadFractalCreator.builder()
-                .samples(1_000_000)
-                .offset(-20)
-                .pointFunctions(List.of(new  DiscFunction(), new SinusoidalFunction()))
-                .iterationsPerSample(100)
+                .samples(properties.getSamples())
+                .offset(properties.getOffset())
+                .pointFunctions(List.of(new DiscFunction(), new SinusoidalFunction()))
+                .iterationsPerSample(properties.getIterationsPerSample())
                 .transformations(affineTransformations)
                 .build();
 
-        PixelCanvas canvas = new PixelCanvas(2000, 2000);
-        fractalGenerator.fillCanvasWithThreads(canvas,6);
+        PixelCanvas canvas = new PixelCanvas(properties.getCanvasHeight(), properties.getCanvasWidth());
+        fractalGenerator.fillCanvasWithThreads(canvas, properties.getThreads());
 
 
         ImageRenderer imageRenderer = new JavaGraphicsImageRenderer();
-        imageRenderer.renderImage(canvas, Path.of("./generatedImages"), "fractalImage", FileFormat.PNG);
+        var imagePath = Path.of(properties.getGeneratedImagesPath());
+        String fileName = properties.getGeneratedImageName();
+        var format = properties.getFileFormat();
+        imageRenderer.renderImage(canvas, imagePath, fileName, format);
     }
 
-    private static List<AffineTransformation> buildListOfAffineTransformations(int n) {
+    Properties getProperties() {
+        var loaderoptions = new LoaderOptions();
+        TagInspector taginspector =
+                tag -> tag.getClassName().equals(Properties.class.getName());
+        loaderoptions.setTagInspector(taginspector);
+        Yaml yaml = new Yaml(new Constructor(Properties.class, loaderoptions));
+        var inputStream = this.getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE_PATH);
+        return yaml.load(inputStream);
+    }
+
+    private List<AffineTransformation> buildListOfAffineTransformations(int n, int tryouts) {
         List<AffineTransformation> transformations = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
-            var transformation = generateAffineTransformation(1_000_000);
+            var transformation = generateAffineTransformation(tryouts);
             transformation.ifPresent(transformations::add);
         }
         return transformations;
     }
 
 
-    private static Optional<AffineTransformation> generateAffineTransformation(int tryouts) {
+    private Optional<AffineTransformation> generateAffineTransformation(int tryouts) {
         for (int tryout = 0; tryout < tryouts; tryout++) {
             double a = ThreadLocalRandom.current().nextDouble(-1, 1);
             double b = ThreadLocalRandom.current().nextDouble(-1, 1);
@@ -73,7 +95,7 @@ public class Main {
         return Optional.empty();
     }
 
-    private static boolean areCorrectAffineTransformationCoefficients(double a, double b, double d, double e) {
+    private boolean areCorrectAffineTransformationCoefficients(double a, double b, double d, double e) {
         return a * a + d * d < 1
                 && b * b + e * e < 1
                 && a * a + b * b + d * d + e * e < 1 + (a * e - b * d) * (a * e - b * d);

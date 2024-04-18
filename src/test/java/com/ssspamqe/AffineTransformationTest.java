4b64b60c41e0b04ffcf3901ec110efcab5b599d9
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-04-18 20:42:17 +0300

    Add test for affine transformation

--- src/test/java/com/ssspamqe/AffineTransformationTest.java
+++ src/test/java/com/ssspamqe/AffineTransformationTest.java
@@ -1,15 +1,35 @@
 package com.ssspamqe;
 
+import com.ssspamqe.fractalgeneration.coordinateobjects.Point;
 import com.ssspamqe.fractalgeneration.pointmodifiers.AffineTransformation;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+
 public class AffineTransformationTest {
+
+    private static final double DEFAULT_PRECISION = 0.01;
 
     @Test
     public void should_performCorrectCalculations() {
         var transformation = AffineTransformation.builder()
                 .a(-0.57)
                 .b(-0.5)
-                .ds
+                .d(0.15)
+                .e(0.08)
+                .c(0)
+                .f(0)
+                .color(new Color(5, 17, 1))
+                .build();
+        var point = new Point(1, 1);
+
+        var actualPoint = transformation.applyOn(point);
+
+        assertThat(actualPoint.x()).isCloseTo(-1.07, withPrecision(DEFAULT_PRECISION));
+        assertThat(actualPoint.y()).isCloseTo(0.23, withPrecision(DEFAULT_PRECISION));
     }
 }
